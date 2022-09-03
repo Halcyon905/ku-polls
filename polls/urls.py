7adc4b870201408b833a21531fa3d111fab433be
Punn Chunwimaleung <khunpunn2546@gmail.com>
2022-09-03 13:03:27 +0700

    removed reliance on hard coded url

--- polls/urls.py
+++ polls/urls.py
@@ -5,7 +5,7 @@ from . import views
 app = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:question_id>/', views.detail, name='detail'),
-    path('<int:question_id>/results/', views.results, name='results'),
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('question/<int:question_id>/', views.detail, name='detail'),
+    path('question/<int:question_id>/results/', views.results, name='results'),
+    path('question/<int:question_id>/vote/', views.vote, name='vote'),
 ]
