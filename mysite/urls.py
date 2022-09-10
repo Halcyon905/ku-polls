9a587ad837877a977464fdc9f900b2415e6d4393
Punn Chunwimaleung <khunpunn2546@gmail.com>
2022-09-10 22:43:40 +0700

    base path redirects to polls/

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
+    path("", lambda request: HttpResponseRedirect('polls/')),
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ]
