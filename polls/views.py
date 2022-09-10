97c7c639d06e35bfdc5610772b4edd6026ac0a10
Punn Chunwimaleung <khunpunn2546@gmail.com>
2022-09-10 22:39:13 +0700

    implemented django error messages

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,10 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
 from .models import Question, Choice
 from django.utils import timezone
+from django.contrib import messages
 
 
 class IndexView(generic.ListView):
@@ -32,12 +33,26 @@ class DetailView(generic.DetailView):
         question = get_object_or_404(Question, pk=pk)
         if question.can_vote():
             return render(request, 'polls/detail.html', {'question': question})
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        elif question.is_published():
+            messages.error(request, 'Voting period is closed for this question.')
+            return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        messages.error(request, 'Access to question denied.')
+        return HttpResponseRedirect(reverse('polls:index'))
 
 
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
+
+    def get(self, request, pk):
+        """
+        Return result page if can_vote method returns True. If not then redirect to results page.
+        """
+        question = get_object_or_404(Question, pk=pk)
+        if question.is_published():
+            return render(request, 'polls/results.html', {'question': question})
+        messages.error(request, 'Access to question denied.')
+        return HttpResponseRedirect(reverse('polls:index'))
 
 
 def vote(request, question_id):
